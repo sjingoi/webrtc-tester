352d2a6e07fadd708074bfacd5c79e29f9280563
Sebastian Jingoi <sebi.jingoi@gmail.com>
2022-10-20 21:52:03 -0400

    Added Connection Message

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 //CONNECTION TYPE
-typeSelection = document.getElementById("connection-type");
-submitTypeBtn = document.getElementById("submit-type");
-submitTypeBtn.onclick = submitType;
+typeSelection = document.getElementById("connection-type")
+submitTypeBtn = document.getElementById("submit-type")
+submitTypeBtn.onclick = submitType
 
 //LOCAL
-submitLocalBtn = document.getElementById("submit-local");
-localOfferBox = document.getElementById("local-offer");
+submitLocalBtn = document.getElementById("submit-local")
+localOfferBox = document.getElementById("local-offer")
 
 //REMOTE
-submitRemoteBtn = document.getElementById("submit-remote");
-remoteOfferBox = document.getElementById("remote-offer");
+submitRemoteBtn = document.getElementById("submit-remote")
+remoteOfferBox = document.getElementById("remote-offer")
 
 //TEXTBOX
-submitTextBtn = document.getElementById("submit-text");
-sendBox = document.getElementById("chat-box-send");
-recieveBox = document.getElementById("chat-box-recieve");
+submitTextBtn = document.getElementById("submit-text")
+sendBox = document.getElementById("chat-box-send")
+recieveBox = document.getElementById("chat-box-recieve")
+connectionMsg = document.getElementById("connection-open-msg")
 
 function submitType() {
     /**
      * Usually executed when the type selector button is pressed.
      */
-    submitTypeBtn.disabled = true;
-    typeSelection.disabled = true;
+    submitTypeBtn.disabled = true
+    typeSelection.disabled = true
+    remoteOfferBox.disabled = false
+
     if (typeSelection.value == "local") {
-        createLocalConnection();
+        createLocalConnection()
     } else {
-        submitRemoteBtn.disabled = false;
-        submitRemoteBtn.onclick = createRemoteConnection;
+        submitRemoteBtn.disabled = false
+        submitRemoteBtn.onclick = createRemoteConnection
     }
 }
 
 function createLocalConnection() {
     const lc = new RTCPeerConnection();
-    const dc = lc.createDataChannel("channel");
-    dc.onmessage = message => recieveBox.textContent = message.data;
+    const dc = lc.createDataChannel("channel")
+
+    localOfferBox.disabled = false
+
+    dc.onmessage = message => recieveBox.textContent = message.data
     dc.onopen = e => {
         console.log("Connection opened.")
         startChatBox(dc);
@@ -42,26 +48,29 @@ function createLocalConnection() {
         console.log("Created new ICE candidate.")
         localOfferBox.textContent = JSON.stringify(lc.localDescription)/*.replace("UDP", "TCP").replace("udp","tcp")*/;
     }
-    lc.createOffer().then(offer => lc.setLocalDescription(offer)).then(a => console.log("Local description set successfully."));
-    submitRemoteBtn.disabled = false;
+    lc.createOffer().then(offer => lc.setLocalDescription(offer)).then(a => console.log("Local description set successfully."))
+    submitRemoteBtn.disabled = false
     submitRemoteBtn.onclick = () => {
-        const answer = JSON.parse(remoteOfferBox.value);
-        lc.setRemoteDescription(answer);
+        submitRemoteBtn.disabled = true
+        const answer = JSON.parse(remoteOfferBox.value)
+        lc.setRemoteDescription(answer)
     }
 }
 
 function createRemoteConnection() {
-    submitRemoteBtn.disabled = true;
-    const offer = JSON.parse(remoteOfferBox.value);
-    const rc = new RTCPeerConnection;
+    submitRemoteBtn.disabled = true
+    localOfferBox.disabled = false
+
+    const offer = JSON.parse(remoteOfferBox.value)
+    const rc = new RTCPeerConnection
     rc.onicecandidate = e => {
-        console.log("Created new ICE candidate.");
-        localOfferBox.textContent = JSON.stringify(rc.localDescription);
+        console.log("Created new ICE candidate.")
+        localOfferBox.textContent = JSON.stringify(rc.localDescription)
     }
     rc.ondatachannel = dataChannel => {
-        rc.dc = dataChannel.channel;
+        rc.dc = dataChannel.channel
         rc.dc.onmessage = message => recieveBox.textContent = message.data;
-        rc.dc.onopen = e => console.log("Connection opened.");
+        rc.dc.onopen = e => console.log("Connection opened.")
         startChatBox(rc.dc);
     }
     rc.setRemoteDescription(offer).then(a => console.log("Offer set."));
@@ -70,8 +79,13 @@ function createRemoteConnection() {
 }
 
 function startChatBox(dataChannel) {
+    sendBox.disabled = false
+    localOfferBox.disabled = true
+    remoteOfferBox.disabled = true
+    connectionMsg.textContent = "Connection Opened. You can now send messages."
+
     submitTextBtn.onclick = () => {
-        dataChannel.send(sendBox.value);
+        dataChannel.send(sendBox.value)
         sendBox.value = "";
     }
 }
